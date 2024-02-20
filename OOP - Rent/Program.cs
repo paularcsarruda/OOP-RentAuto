6ec25091d3a841317f27fca2fa1bbe60a727d84b
Paula Arruda <122739036+paularcsarruda@users.noreply.github.com>
2024-02-20 09:28:50 -0300

    Update Program.cs
    Correção de erro.

--- OOP - Rent/Program.cs
+++ OOP - Rent/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices;
 
 namespace OOP___Rent
@@ -7,29 +7,46 @@ namespace OOP___Rent
     {
         static void Main(string[] args)
         {
+            bool sair = false;
             int numDiarias = 10;
-            Console.WriteLine("|+---------------------------+|");
-            Console.WriteLine("| Qual Veiculo deseja Alugar? |");
-            Console.WriteLine("| [1] Carro                   |");
-            Console.WriteLine("| [2] Moto                    |");
-            Console.WriteLine("|+---------------------------+|");
-            ConsoleKeyInfo opcao = Console.ReadKey(true);
-            Console.ReadLine();
 
-            if (opcao.KeyChar == '1')
+            while (!sair)
             {
-                Carro carro = new Carro();
-                double valorTotalCarro = carro.CalcularValorTotal(numDiarias);
-                Console.WriteLine($"O Valor total à pagar para 10 diárias de carro: R$ {valorTotalCarro:F2}");
-            }
-            else if (opcao.KeyChar == '2')
-            {
-                Moto moto = new Moto();
-                double valorTotalMoto = moto.CalcularValorTotal(numDiarias);
-                Console.WriteLine($"O Valor total à pagar para 10 diárias de moto: R$ {valorTotalMoto:F2}");
-            }
+                Console.WriteLine("|+---------------------------+|");
+                Console.WriteLine("| Qual Veiculo deseja Alugar? |");
+                Console.WriteLine("| [1] Carro                   |");
+                Console.WriteLine("| [2] Moto                    |");
+                Console.WriteLine("| [0] Sair                    |");
+                Console.WriteLine("|+---------------------------+|");
+                ConsoleKeyInfo opcao = Console.ReadKey(true);
+                Console.ReadLine();
+                Console.Clear();
 
-            Console.ReadLine();
+                switch (opcao.KeyChar)
+                {
+                    case '1':
+                        Carro carro = new Carro();
+                        double valorTotalCarro = carro.CalcularValorTotal(numDiarias);
+                        Console.WriteLine($"Valor total à pagar para 10 diárias de carro: R$ {valorTotalCarro:F2}");
+                        Console.ReadLine();
+                        break;
+                    case '2':
+                        Moto moto = new Moto();
+                        double valorTotalMoto = moto.CalcularValorTotal(numDiarias);
+                        Console.WriteLine($"Valor total à pagar para 10 diárias de moto: R$ {valorTotalMoto:F2}");
+                        break;
+                    case '0':
+                        sair = true;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida. Por favor, escolha novamente.");
+                        break;
+                }
+
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
     }
 }
